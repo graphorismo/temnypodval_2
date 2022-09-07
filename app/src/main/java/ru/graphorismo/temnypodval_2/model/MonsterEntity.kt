fee5e2fbc5bafb0a04eba1c3fff716141d6429c3
graphorismo <graphorismo@gmail.com>
2022-09-07 18:31:05 +0300

    cosmetic changes

--- app/src/main/java/ru/graphorismo/temnypodval_2/model/MonsterEntity.kt
+++ app/src/main/java/ru/graphorismo/temnypodval_2/model/MonsterEntity.kt
@@ -2,7 +2,11 @@ package ru.graphorismo.temnypodval_2.model
 
 class MonsterEntity(var name: String, health: Int, imageId: Int): AliveEntity(health, imageId) {
     override fun getSupportInfo(): String {
-        return "THATS ${name}! THIS MONSTER WILL HIT YOU"
+        return "MONSTER ${name} WILL HIT YOU!"
+    }
+
+    override fun getMainInfo(): String {
+        return "MONSTER HEALTH $health"
     }
 
     override fun clone(): AEntity {
--- app/src/main/java/ru/graphorismo/temnypodval_2/model/PlayerEntity.kt
+++ app/src/main/java/ru/graphorismo/temnypodval_2/model/PlayerEntity.kt
@@ -2,7 +2,11 @@ package ru.graphorismo.temnypodval_2.model
 
 class PlayerEntity(var score: Int, health: Int, imageId: Int) : AliveEntity(health, imageId) {
     override fun getSupportInfo(): String {
-        return "SCORE: $score"
+        return "PLAYER SCORE: $score"
+    }
+
+    override fun getMainInfo(): String {
+        return "PLAYER HEALTH $health"
     }
 
     override fun clone(): AEntity {
